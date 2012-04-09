aa895b8f0119bc6ea33473f268027ca080b49b19
Mike Gardiner <mgardin@thoughtworks.com>
2012-04-09 21:39:09 +0800

    Added MongoHQ configuration for production

--- app/mongoid_config.rb
+++ app/mongoid_config.rb
@@ -1,5 +1,11 @@
 require 'mongoid'
 
 Mongoid.configure do |config|
-	config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db("test")
+	if ENV['MONGOHQ_URL']
+		connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+		uri = URI.parse(ENV['MONGOHQ_URL'])
+		config.master = connection.db(uri.path.gsub(/^\//, ''))
+	else
+		config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db("test")
+	end
 end
