007269318fc06e8cc9af00da03ff1e04bc98dbe5
Mike Gardiner <mgardin@thoughtworks.com>
2012-04-11 22:54:21 +0800

    Simplified requiring application controllers and models

--- app/hansel.rb
+++ app/hansel.rb
@@ -3,10 +3,6 @@ require 'mongoid'
 require 'sinatra-authentication'
 
 require File.join(File.dirname(__FILE__), 'mongoid_config')
-
-require File.join(File.dirname(__FILE__), 'models', 'gist')
-require File.join(File.dirname(__FILE__), 'controllers', 'mails_controller')
-require File.join(File.dirname(__FILE__), 'controllers', 'gists_controller')
-require File.join(File.dirname(__FILE__), 'controllers', 'auth_controller')
+Dir.glob(File.join(File.dirname(__FILE__), "{controllers,models}", "*.rb")) { |f| require f }
 
 use Rack::Session::Cookie, :secret => 'Or did you think I was too stupid to know what a eugoogooly was?'
