5cc11df5666827758b597a4bf1e00c5ae0603046
gardym <mgardin@thoughtworks.com>
2012-04-30 22:43:58 +0800

    Moved the linkify helper out from the gists_controller

--- app/hansel.rb
+++ app/hansel.rb
@@ -3,6 +3,6 @@ require 'mongoid'
 require 'sinatra-authentication'
 
 require File.join(File.dirname(__FILE__), 'mongoid_config')
-Dir.glob(File.join(File.dirname(__FILE__), "{controllers,models}", "*.rb")) { |f| require f }
+Dir.glob(File.join(File.dirname(__FILE__), "{controllers,models,helpers}", "*.rb")) { |f| require f }
 
 use Rack::Session::Cookie, :secret => 'Or did you think I was too stupid to know what a eugoogooly was?'
---
+++ app/helpers/linkify_helper.rb
@@ -0,0 +1,11 @@
+helpers do
+	def linkify( text )
+		generic_URL_regexp = Regexp.new( '(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
+		starts_with_www_regexp = Regexp.new( '(^|[\n ])((www)\.[^ \"\t\n\r<]*)', Regexp::MULTILINE | Regexp::IGNORECASE )
+
+	  s = text.to_s
+	  s.gsub!( generic_URL_regexp, '\1<a target="_blank" href="\2">\2</a>' )
+	  s.gsub!( starts_with_www_regexp, '\1<a target="_blank" href="http://\2">\2</a>' )
+	  s
+	end
+end
