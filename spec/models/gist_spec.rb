66db0d82b2b0961474129240ae472df11cf71be8
Mike Gardiner <conversationing@gmail.com>
2012-06-09 12:25:48 +0800

    Layout fixes in gist_spec

--- spec/models/gist_spec.rb
+++ spec/models/gist_spec.rb
@@ -1,19 +1,14 @@
 require 'spec_helper'
 
 describe "Gist Model" do
-
   describe "Creating Gists" do
-
     it "should persist the Gist" do
-
       title = "I'm not an ambi-turner"
-
       Gist.create(:title => title)
+
       gist = Gist.where(:title => title)
+
       gist.first.title.should == title
-
     end
-
   end
-
 end
