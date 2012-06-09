23497f12843c474a0e9ddabcb9f030e3c5148d48
Mike Gardiner <conversationing@gmail.com>
2012-06-09 12:28:15 +0800

    :retab

--- app/models/gist.rb
+++ app/models/gist.rb
@@ -1,11 +1,11 @@
 require 'mongoid'
 
 class Gist
-	include Mongoid::Document
+  include Mongoid::Document
 
-	field :title, type: String
-	field :link, type: String
-	field :text, type: String
-	field :done, type: Boolean
-	field :created_at, type: DateTime
+  field :title, type: String
+  field :link, type: String
+  field :text, type: String
+  field :done, type: Boolean
+  field :created_at, type: DateTime
 end
--- app/public/hansel-main.js
+++ app/public/hansel-main.js
@@ -1,37 +1,36 @@
 require(["jquery"], function($) {
-    $(function() {
+  $(function() {
+    var redirectWithQueryString = function(destination) {
+      window.location.href = destination + window.location.search;
+    };
 
-        var redirectWithQueryString = function(destination) {
-          window.location.href = destination + window.location.search;
-        };
+    var redirectWithOrder = function(order) {
+      window.location.href = window.location.pathname + "?order_by=" + order;
+    };
 
-        var redirectWithOrder = function(order) {
-          window.location.href = window.location.pathname + "?order_by=" + order;
-        };
-
-        $("button.all").on("click", function() {
-          redirectWithQueryString("/gists/all");
-        });
-
-        $("button.not-done").on("click", function() {
-          redirectWithQueryString("/gists");
-        });
-
-        $("span.sort select").on("change", function() {
-          redirectWithOrder($(this).val());
-        });
-
-        $("input.done").on("click", function() {
-          var li = $(this).closest("li");
-          var done = $(this).attr("checked") !== undefined;
-
-          $.ajax("/gist/" + $(this).val(), {
-            type: "PUT",
-            data: { gist: { done: done } },
-            complete: function() {
-              done ? li.addClass("gist-done") : li.removeClass("gist-done");
-            }
-          });
-        });
+    $("button.all").on("click", function() {
+      redirectWithQueryString("/gists/all");
     });
+
+    $("button.not-done").on("click", function() {
+      redirectWithQueryString("/gists");
+    });
+
+    $("span.sort select").on("change", function() {
+      redirectWithOrder($(this).val());
+    });
+
+    $("input.done").on("click", function() {
+      var li = $(this).closest("li");
+      var done = $(this).attr("checked") !== undefined;
+
+      $.ajax("/gist/" + $(this).val(), {
+        type: "PUT",
+        data: { gist: { done: done } },
+        complete: function() {
+          done ? li.addClass("gist-done") : li.removeClass("gist-done");
+        }
+      });
+    });
+  });
 });
