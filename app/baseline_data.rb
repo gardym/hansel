605c72ea5894f724937c7bc290d788bd6d33c541
Mike Gardiner <conversationing@gmail.com>
2012-06-09 12:27:03 +0800

    Remove all those trailing prepositions

--- app/baseline_data.rb
+++ app/baseline_data.rb
@@ -2,7 +2,7 @@ require File.join(File.dirname(__FILE__), 'hansel')
 
 module Baseline
   def self.setup(email, password)
-    raise "Can't insert baseline data without email and password set" if (email.nil? || password.nil?)
+    raise "Can't insert baseline data without email and password." if (email.nil? || password.nil?)
 
     user = MongoidUser.create(:email => email)
     user.password = password
