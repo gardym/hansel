65a19d52340b4092962b2c8770f7120468dd841c
gardym <mgardin@thoughtworks.com>
2012-08-26 16:28:41 +0800

    Additional configuration for MongoHQ on Heroku.

--- app/hansel.rb
+++ app/hansel.rb
@@ -2,7 +2,8 @@ require 'sinatra'
 require 'mongoid'
 require 'sinatra-authentication'
 
-require File.join(File.dirname(__FILE__), 'mongoid_config')
+Mongoid.load!(File.join(settings.root, 'mongoid.yml'))
+
 Dir.glob(File.join(File.dirname(__FILE__), "{controllers,models,helpers}", "*.rb")) { |f| require f }
 
 use Rack::Session::Cookie, :secret => 'Or did you think I was too stupid to know what a eugoogooly was?'
--- app/mongoid_config.rb
+++
@@ -1,9 +0,0 @@
-require 'mongoid'
-
-Mongoid.configure do |config|
-  if ENV['MONGOHQ_URL']
-    Mongoid.load!(File.join(settings.root, 'mongoid.yml'), :production)
-  else
-    Mongoid.load!(File.join(settings.root, 'mongoid.yml'), :development)
-  end
-end
