8f65779bbfb8472ab6386a928188e382edee823d
Mike Gardiner <conversationing@gmail.com>
2014-01-01 20:07:07 +0000

    /gists and /gists/all should *basically* do the same thing.

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -1,19 +1,23 @@
 get '/gists' do
-  throw(:warden) unless env['warden'].authenticated?
-
-  @gists = apply_order(Gist.where(:done => false))
-  @tags = @gists.select { |g| not g.tags.nil? }.map { |g| g.tags }.flatten.sort.uniq
-  haml :list, :format => :html5
+  list { Gist.where(:done => false) }
 end
 
 get '/gists/all' do
-  @gists = apply_order(Gist.all)
-  haml :list, :format => :html5
+  list { Gist.all }
 end
 
 put '/gist/:id' do
   gist = Gist.find(params[:id])
   gist.update_attributes(params[:gist])
+end
+
+def list
+  throw(:warden) unless env['warden'].authenticated?
+
+  @gists = apply_order(yield)
+  @tags = load_unique_tags
+
+  haml :list, :format => :html5
 end
 
 def apply_order(gists)
@@ -26,4 +30,8 @@ def apply_order(gists)
 
   @order_by = order.join(",")
   gists.order_by(order)
+end
+
+def load_unique_tags
+  @gists.select { |g| not g.tags.nil? }.map { |g| g.tags }.flatten.sort.uniq
 end
